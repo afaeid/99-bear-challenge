d897a4489aa98676275d03aed74913f5e264cc66
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2023-11-22 20:33:03 +0600

    code cleaned.

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 var verseShower = document.querySelector(".verse-shower");
 var remainBears = 99;
 var i;
+
+const delay = (ms) => {
+ return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const displayVerse = async () => {
  for (i = remainBears; i >= 0; i--) {
-  
-  var bottleStatues = i > 1 ? "bottles" : "bottle";
-  var remainBottleStatues = i-1 > 1 ? "bottles" : "bottle";
-  var firstVerse = i > 0 ?  `${i} ${bottleStatues} of beer on the wall. ${i} ${bottleStatues} of beer. Take one down, pass it around, ${i - 1} ${remainBottleStatues} of beer on the wall.` : "There is no more bear. But you go to the store and buy more bear."
-  var eachWords = firstVerse.split(" ");
+
+  var bottleStatus = i > 1 ? "bottles" : "bottle";
+  var remainBottleStatus = i - 1 > 1 ? "bottles" : "bottle";
+  var verse = i > 0 ? `${i} ${bottleStatus} of beer on the wall. ${i} ${bottleStatus} of beer. Take one down, pass it around, ${i - 1} ${remainBottleStatus} of beer on the wall.` : "There is no more bear. But you go to the store and buy more bear."
+  var eachWords = verse.split(" ");
   var verseElm = document.createElement("div");
   verseElm.classList.add("verse");
   verseShower.appendChild(verseElm);
@@ -25,10 +30,6 @@ const displayVerse = async () => {
   }
 
  }
-}
-
-const delay = (ms) => {
- return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 displayVerse();
